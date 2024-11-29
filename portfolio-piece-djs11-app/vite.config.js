47e634e4c42fb56897419deabf2e7f6baa51ef08
Nthabiseng Mashalane <nthabimashalane0@gmail.com>
2024-11-29 14:49:41 +0200

    Committed Changes

--- portfolio-piece-djs11-app/vite.config.js
+++ portfolio-piece-djs11-app/vite.config.js
@@ -4,4 +4,8 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/', // Ensure your app works correctly if deployed to root (default for Netlify)
+  build: {
+    outDir: 'dist', // Ensure the build output directory is set to 'dist'
+  },
 })
